2e8ba14ddd80f0908b600a2f2318bd1f4b750621
Kartik Sareen <47638793+KartikSareen18@users.noreply.github.com>
2020-06-04 10:30:48 +0530

    Server Side Development

--- confusionServer/config.js
+++ confusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '179050993468415',
-        clientSecret: '4d9a85877fefa57eda4b3260d1b15c5e'
+        clientId: 'FACEBOOK CLIENT ID',
+        clientSecret: 'FACEBOOK CLIENT SECRET'
     }
 }
